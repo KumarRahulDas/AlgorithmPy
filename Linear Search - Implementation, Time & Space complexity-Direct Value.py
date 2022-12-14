eb24d9a4bf1995ba49194628e0f9ea82a408f47c
KumarRahulDas <dasrahulbxrepa7061503554@gmail.com>
2022-12-14 01:47:15 -0800

    Code Done

---
+++ Linear Search - Implementation, Time & Space complexity-Direct Value.py
@@ -0,0 +1,12 @@
+def linearSearch(arr,size,key):
+    for i in range(size):
+       if arr[i]==key:
+        return 1
+    return 0
+if __name__ =="__main__":
+    page_number = 20,30,40,50,60
+    key = 50
+    if linearSearch(page_number,len(page_number),key) ==1:
+        print("search found")
+    else:
+        print("search not found")
